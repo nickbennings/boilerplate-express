a8bed3a07fa2152dd1b3f8dc31ef157ebb650e18
nickbennings <143043766+nickbennings@users.noreply.github.com>
2024-05-16 21:49:55 -0600

    Update tests

--- myApp.js
+++ myApp.js
@@ -1,8 +1,18 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
 
+// Serve static assets from the /public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Set up a route to serve the index.html file
+app.get('/', function(req, res) {
+  let absolutePath = path.join(__dirname, 'views', 'index.html');
+  res.sendFile(absolutePath);
+});
 
+// Do not edit below this line
+module.exports = app;
 
 
 
