5cd0f24e9d21a9791456c8c2a0fdaaf3031d443b
nickbennings <143043766+nickbennings@users.noreply.github.com>
2024-05-16 22:03:08 -0600

    Resolved merge conflict in myApp.js

--- myApp.js
+++ myApp.js
@@ -2,15 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Serve static assets from the /public directory
-app.use(express.static(path.join(__dirname, 'public')));
-console.log("Hello World");
-
-// Set up a route to serve the index.html file
-app.get('/', function(req, res) {
-  let absolutePath = path.join(__dirname, 'views', 'index.html');
-  res.sendFile(absolutePath);
-});
+// Mount the express.static() middleware to serve static assets from the /public directory
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Do not edit below this line
 module.exports = app;
